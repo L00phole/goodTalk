4ad3ec80381ba484b80c5ead260a93fbd203f18d
Michele <vilda1002@gmail.com>
2023-05-25 11:34:27 +0200

    new

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,13 +2,11 @@ import axios from "axios";
 import { getUserFromLocalStorage } from "./localstorage";
 
 const api = axios.create();
-const api = axios.create();
 
 api.interceptors.request.use((config) => {
   const user = getUserFromLocalStorage();
   if (user) {
     config.headers["authorization"] = `Bearer ${user.token}`;
-    config.headers["Authorization"] = `Bearer ${user.token}`;
   }
   return config;
 });
